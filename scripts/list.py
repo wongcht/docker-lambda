33ad46cbb1f0ebd322112d112e9be7565dffd58b
wongcht <16640500+wongcht@users.noreply.github.com>
2026-02-18 16:34:25 +0000

    format list.py

--- scripts/list.py
+++ scripts/list.py
@@ -1,5 +1,5 @@
-
 import json
+
 from boto3.session import Session as boto3_session
 
 AWS_REGIONS = [
@@ -44,15 +44,15 @@ def main():
         for layer in layers:
             response = client.list_layer_versions(LayerName=layer)
             latest = response["LayerVersions"][0]
-            res["layers"].append(dict(
-                name=layer,
-                arn=latest["LayerVersionArn"],
-                version=latest["Version"]
-            ))
+            res["layers"].append(
+                dict(
+                    name=layer, arn=latest["LayerVersionArn"], version=latest["Version"]
+                )
+            )
         results.append(res)
 
     print(json.dumps(results))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
